67931fb8eee2054353c126aa4a33b9e4d8fd4f6e
Marshal <marshal.wu@qq.com>
2015-05-15 00:17:44 +0800

    初步实现flux的示例

--- js/comments.js
+++ js/comments.js
@@ -2,11 +2,12 @@
 var React = require('react');
 var Dispatcher = require('flux').Dispatcher;
 var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
 
-alert(EventEmitter);
+var AppDispatcher=new Dispatcher();
 
-var CommentBox=React.createClass({
-	comments:[
+var CommentStore=assign({}, EventEmitter.prototype, {
+	items:[
 		{
 			title:'不错，超级满意',
 			content:'样子漂亮，买之前还在担心有什么问题，是我多想了，很实用，很好'
@@ -15,12 +16,34 @@ var CommentBox=React.createClass({
 			title:'还不错',
 			content:'送人的礼物，很满意的购物'
 		}
-	],
+	]
+});
+
+AppDispatcher.register(function(payload){
+	switch( payload.eventName ) {
+	    case 'new-item':
+	    	CommentStore.items.push( payload.newItem );
+	    	CommentStore.emit('change');
+	      	break;
+  	}
+  	return true;
+});
+
+var CommentBox=React.createClass({
+	getInitialState: function() {
+		var self=this;
+		CommentStore.on('change',function(){
+			self.setState(CommentStore.items);
+		});
+		return {data:CommentStore.items};
+	},
 	render:function(){
 		return (
 			<div className="commentBox">
 				<h1>iPad mini 3的评论</h1>
-				<CommentList data={this.comments}/>
+				<CommentList data={this.state.data}/>
+				<hr />
+				<CommentForm />
 			</div>
 		);
 	}
@@ -29,12 +52,12 @@ var CommentBox=React.createClass({
 var CommentList=React.createClass({
 	render:function(){
 		var commentNodes=this.props.data.map(
-			function(comment,index){
+			function(comment){
 				return (
-					<p>
+					<div>
 						<h3>{comment.title}</h3>
 						<div>{comment.content}</div>
-					</p>
+					</div>
 				);
 			}
 		);
@@ -43,6 +66,35 @@ var CommentList=React.createClass({
 			<div className="commentList">
 				{commentNodes}
 			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var title = this.refs.title.getDOMNode().value.trim();
+		var content = this.refs.content.getDOMNode().value.trim();
+		if (!title || !content) {
+			return;
+		}
+		// this.props.onCommentSubmit({title: title, content: content});
+		this.refs.title.getDOMNode().value = '';
+		this.refs.content.getDOMNode().value = '';
+
+		AppDispatcher.dispatch({
+			eventName: 'new-item',
+			newItem:{title: title, content: content}
+		});
+		return;
+	},
+	render: function() {
+		return (
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input type="text" placeholder="title" ref="title" /><br/>
+				<input type="text" placeholder="content..." ref="content" /><br/>
+				<input type="submit" value="提交" />
+			</form>
 		);
 	}
 });
